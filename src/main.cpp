99e17a4f567caafe2bc95716f3a8f5ffffe5c99b
root <root@Jon.>
2025-05-31 10:02:10 -0700

    Set terminal title to filname for tmux/terminal support

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,24 @@
 #include "../include/app.h"
 #include <iostream>
 
-int main(int argc, char* argv[]) {
-    App app;
+int main(int argc, char *argv[]) {
+  App app;
+  std::string fileName;
 
-    if (argc > 1) {
-        std::string fileName = argv[1];
-        app.openFile(fileName);
-    } else {
-        std::cout << "Please use ./nimp filename.txt" << std::endl;
-        return 1;
-    }
+  if (argc > 1) {
+    std::string fileName = argv[1];
+    app.openFile(fileName);
+  } else {
+    std::cout << "Please use ./nimp filename.txt" << std::endl;
+    return 1;
+  }
+  const char *tmux_env = std::getenv("TMUX");
+  const char *screen_env = std::getenv("STY");
+  if (tmux_env || screen_env) {
+    std::string titleC = "printf '\\033]2;" + fileName + "\\007'";
+    std::system(titleC.c_str());
+  }
 
-    app.start();
-    return 0;
+  app.start();
+  return 0;
 }
